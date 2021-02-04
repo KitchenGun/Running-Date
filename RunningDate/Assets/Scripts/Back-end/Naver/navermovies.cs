7ef93a0df163e5afb6eed04743246f3b915bc30c
KitchenGun <kitchengun124@gmail.com>
2021-02-04 23:42:42 +0900

    Image PopUp sample
    add sample code

--- RunningDate/Assets/Scripts/Back-end/Naver/navermovies.cs
+++ RunningDate/Assets/Scripts/Back-end/Naver/navermovies.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System;
 using System.Net;
 using System.Text;
@@ -9,59 +10,62 @@ using Newtonsoft.Json.Linq;
 
 public class navermovies : MonoBehaviour
 {
-    public class APIExamSearchBlogg
+    public GameObject Image;
+
+    private void ImageSet(string url)
     {
+        //Image.GetComponent<Image>().sprite=
+    }
 
-        private void Start()
+    public void Input()
+    {
+        Debug.Log("Search a movie : ");
+        string query = "해리";
+        string url = "https://openapi.naver.com/v1/search/movie?query=" + query + "&display=5";
+
+
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+        request.Headers.Add("X-Naver-Client-Id", "pD3FuZnjsrm1nXgP8isw");
+        request.Headers.Add("X-Naver-Client-Secret", "wZOutcA8wg");
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        string status = response.StatusCode.ToString();
+        if (status == "OK")
         {
-            Console.WriteLine("Search a movie : ");
-            string query = Console.ReadLine();
-            string url = "https://openapi.naver.com/v1/search/movie?query=" + query + "&display=5";
+            Stream stream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+            string text = reader.ReadToEnd();
+            //Console.WriteLine(text);
 
+            //List<movieLists> m =JsonConvert.DeserializeObject<List<movieLists>>(text);
+            JObject obj = JObject.Parse(text);
+            JArray array = JArray.Parse(obj["items"].ToString());
 
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-            request.Headers.Add("X-Naver-Client-Id", "pD3FuZnjsrm1nXgP8isw");
-            request.Headers.Add("X-Naver-Client-Secret", "wZOutcA8wg");
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            string status = response.StatusCode.ToString();
-            if (status == "OK")
+            Debug.Log("The movies are as follow !!");
+
+            foreach (JObject itemObj in array)
             {
-                Stream stream = response.GetResponseStream();
-                StreamReader reader = new StreamReader(stream, Encoding.UTF8);
-                string text = reader.ReadToEnd();
-                //Console.WriteLine(text);
-
-                //List<movieLists> m =JsonConvert.DeserializeObject<List<movieLists>>(text);
-                JObject obj = JObject.Parse(text);
-                JArray array = JArray.Parse(obj["items"].ToString());
-
-                Console.WriteLine("The movies are as follow !!");
-
-                foreach (JObject itemObj in array)
-                {
-                    movieLists movies = new movieLists();
-                    movies.korname = itemObj["title"].ToString();
-                    Console.WriteLine("Korean name : " + movies.korname);
-                    movies.engname = itemObj["subtitle"].ToString();
-                    Console.WriteLine("English name : " + movies.engname);
-                    movies.publishDate = itemObj["pubDate"].ToString();
-                    Console.WriteLine("Published Date : " + movies.publishDate);
-                    movies.director = itemObj["director"].ToString();
-                    Console.WriteLine("Director name : " + movies.director);
-                    movies.link = itemObj["link"].ToString();
-                    Console.WriteLine("Movie Link : " + movies.link);
-                    Console.WriteLine("==================================");
-
-                }
-
+                APIExamSearchBlogg.movieLists movies = new APIExamSearchBlogg.movieLists();
+                movies.korname = itemObj["title"].ToString();
+                Debug.Log("Korean name : " + movies.korname);
+                movies.engname = itemObj["subtitle"].ToString();
+                Debug.Log("English name : " + movies.engname);
+                movies.publishDate = itemObj["pubDate"].ToString();
+                Debug.Log("Published Date : " + movies.publishDate);
+                movies.director = itemObj["director"].ToString();
+                Debug.Log("Director name : " + movies.director);
+                movies.link = itemObj["link"].ToString();
+                Debug.Log("Movie Link : " + movies.link); ;
             }
-            else
-            {
-                Console.WriteLine("Error " + status);
-            }
-
 
         }
+        else
+        {
+            Debug.Log("Error " + status);
+        }
+    }
+
+    public class APIExamSearchBlogg
+    {
         public class movieLists
         {
             public string korname { get; set; }
