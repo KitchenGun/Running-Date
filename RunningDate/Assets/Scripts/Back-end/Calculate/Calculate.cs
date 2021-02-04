fd549d51445fc74b6e284d8411f57f1c23847495
KitchenGun <kitchengun124@gmail.com>
2021-02-04 20:03:32 +0900

    상영 확률 연산 식 제작
    1주 부터 5주가 나오도록 제작
    미구현:
    영화정보 불러오는 코드
    DB연결코드

--- RunningDate/Assets/Scripts/Back-end/Calculate/Calculate.cs
+++ RunningDate/Assets/Scripts/Back-end/Calculate/Calculate.cs
@@ -8,30 +8,63 @@ public class Calculate : MonoBehaviour
     //select in naver
     private string MovieName;
     //find in db
-    private int Week;
-    private float ReservationRate;
-    private float SalesShare;
+    private float ThisSalesShare;
+    private float BeginSalesShare;
     private int Audience;
     private int Screen;
     #endregion
     #region Select Region
     private string Region;
     #endregion
+    #region CalculateDate()
+    private float SquaredX;
+    private float[] Week;// 주차별 확률값이 저장이 됨
+    #endregion
 
 
+    private void Start()
+    {
+        //Reset
+        Week = new float[5];
+    }
 
-    #region Select Movie
+    #region Select Movie Func
     public void SelectMovie(string name)
     {
         MovieName = name;
     }
     #endregion
 
-    #region
+    #region Select Region Func
     public void SelectRegion(string name)
     {
-
+        Region = name;
     }
     #endregion
 
+    #region Calculate Func
+
+    //(개봉주의 판매량과 이번 주 매출액점유율)*3.5+(관객수/상영횟수)*60 
+    public void CalculateSquaredX()
+    {
+        SquaredX = ((ThisSalesShare + BeginSalesShare) * 3.5f) + (Audience / Screen) * (1f / 60);
+        SquaredX = Mathf.Round(SquaredX * 100) * 0.01f;
+        Debug.Log(SquaredX);
+    }
+    //주차별 결과값
+    public void CalculateResult()
+    {
+        for (int i = 1; i < 6; i++)
+        {
+            float x = Mathf.Pow(10, (SquaredX + 1f));
+            x = Mathf.Round(x * 100) * 0.01f;
+            float y = Mathf.Pow(i, 6f) + Mathf.Pow(10, SquaredX);
+            y = Mathf.Round(y * 100) * 0.01f;
+            Week[i - 1] = Mathf.Round((x / y) * 10) * 0.1f;
+            //확인용 debug log
+            Debug.Log(i + "주차" + "" + Week[i - 1]);
+        }
+    }
+    
+    #endregion
 }
