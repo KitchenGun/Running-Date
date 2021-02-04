886f51d6f237d438632b7489c1b0f6149a9df489
sooyong <wjdtndyd7@naver.com>
2021-02-04 22:46:26 +0900

    싱글톤구조로Manager제작

---
+++ RunningDate/Assets/Scripts/Front-end/Manager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Manager : MonoBehaviour
+{
+    private static Manager instance = null;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (null == instance)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    #region go MovieSelect
+    public void MovieSelect ()
+    {
+        SceneManager.LoadScene(1);
+    }
+    #endregion
+
+}
