f25e53b82ee58b25007bfbbc74b4fb38157251ac
KitchenGun <kitchengun124@gmail.com>
2021-02-04 17:51:52 +0900

    Calculate class add
    변수생성

---
+++ RunningDate/Assets/Scripts/Back-end/Calculate/Calculate.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Calculate : MonoBehaviour
+{
+    #region Movie info
+    //select in naver
+    private string MovieName;
+    //find in db
+    private int Week;
+    private float ReservationRate;
+    private float SalesShare;
+    private int Audience;
+    private int Screen;
+    #endregion
+    #region Select Region
+    private string Region;
+    #endregion
+
+
+
+    #region Select Movie
+    public void SelectMovie(string name)
+    {
+        MovieName = name;
+    }
+    #endregion
+
+    #region
+    public void SelectRegion(string name)
+    {
+
+    }
+    #endregion
+
+}
