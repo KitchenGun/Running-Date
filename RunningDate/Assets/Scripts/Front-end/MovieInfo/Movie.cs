7a83912c4eed3423ee7dc2f2c69a4f35d57fd68a
KitchenGun <kitchengun124@gmail.com>
2021-02-05 16:43:17 +0900

    movieClass edit
    image delete

--- RunningDate/Assets/Scripts/Front-end/MovieInfo/Movie.cs
+++ RunningDate/Assets/Scripts/Front-end/MovieInfo/Movie.cs
@@ -4,13 +4,7 @@ using UnityEngine;
 
 public class Movie : MonoBehaviour
 {
-    public string Image { get; private set; }
     public string Name { get; private set; }
     public int UserRating { get; private set; }
     public string DirectorName { get; private set; }
-
-    private void DataUpdate()
-    {
-        
-    }
 }
