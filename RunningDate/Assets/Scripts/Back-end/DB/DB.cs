ca3b7e793725ee72a8696e715590a09f991e0b84
akdcls1 <56528690+akdcls1@users.noreply.github.com>
2021-02-06 07:25:56 +0900

    중간세이브
    되기를빌며

--- RunningDate/Assets/Scripts/Back-end/DB/DB.cs
+++ RunningDate/Assets/Scripts/Back-end/DB/DB.cs
@@ -9,44 +9,261 @@ using System.Collections.Generic;
 
 public class DB : MonoBehaviour
 {
+    private Calculate calculate;
+    private int Audience;
+    private int Screen;
+    private float BeginSalesShare;
+    private float ThisSalesShare;
+    private string MovieName;
+
+    int a = 0;
+
     void Start()
     {
+        calculate = this.gameObject.GetComponent<Calculate>();
+        MovieName = calculate.MovieName;
+
         DBConnectionCheck();
-        DataBaseRead("Select * From DB");
+
+        if (MovieName == "소울")
+        {
+            DataBaseThisRead("Select * From All_1_4 WHERE Ranking LIKE 1 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Busan_1_4 WHERE Ranking LIKE 1 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Chungbuk_1_4 WHERE Ranking LIKE 1 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Chungnam_1_4 WHERE Ranking LIKE 1 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Daegu_1_4 WHERE Ranking LIKE 1 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Daejeon_1_4 WHERE Ranking LIKE 1 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Gangwon_1_4 WHERE Ranking LIKE 1 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Gwangju_1_4 WHERE Ranking LIKE 1 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Gyeongbuk_1_4 WHERE Ranking LIKE 1 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Gyeonggi_1_4 WHERE Ranking LIKE 1 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Gyeongnam_1_4 WHERE Ranking LIKE 1 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Incheon_1_4 WHERE Ranking LIKE 1 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Jeju_1_4 WHERE Ranking LIKE 1 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Jeonbuk_1_4 WHERE Ranking LIKE 1 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Jeonnam_1_4 WHERE Ranking LIKE 1 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Sejong_1_4 WHERE Ranking LIKE 1 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Seoul_1_4 WHERE Ranking LIKE 1 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Ulsan_1_4 WHERE Ranking LIKE 1 ");
+            calsend2();
+
+        }
+        else if (MovieName == "극장판 귀멸의 칼날: 무한열차편")
+        {
+            DataBaseThisRead("Select * From All_1_4 WHERE Ranking LIKE 2 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Busan_1_4 WHERE Ranking LIKE 2 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Chungbuk_1_4 WHERE Ranking LIKE 2 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Chungnam_1_4 WHERE Ranking LIKE 2 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Daegu_1_4 WHERE Ranking LIKE 2 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Daejeon_1_4 WHERE Ranking LIKE 2 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Gangwon_1_4 WHERE Ranking LIKE 2 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Gwangju_1_4 WHERE Ranking LIKE 2 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Gyeongbuk_1_4 WHERE Ranking LIKE 2 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Gyeonggi_1_4 WHERE Ranking LIKE 2 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Gyeongnam_1_4 WHERE Ranking LIKE 2 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Incheon_1_4 WHERE Ranking LIKE 2 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Jeju_1_4 WHERE Ranking LIKE 2 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Jeonbuk_1_4 WHERE Ranking LIKE 2 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Jeonnam_1_4 WHERE Ranking LIKE 2 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Sejong_1_4 WHERE Ranking LIKE 2 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Seoul_1_4 WHERE Ranking LIKE 2 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Ulsan_1_4 WHERE Ranking LIKE 2 ");
+            calsend2();
+        }
+        else if (MovieName == "세자매")
+        {
+            DataBaseThisRead("Select * From All_1_4 WHERE Ranking LIKE 3 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Busan_1_4 WHERE Ranking LIKE 3 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Chungbuk_1_4 WHERE Ranking LIKE 3 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Chungnam_1_4 WHERE Ranking LIKE 3 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Daegu_1_4 WHERE Ranking LIKE 3 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Daejeon_1_4 WHERE Ranking LIKE 3 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Gangwon_1_4 WHERE Ranking LIKE 3 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Gwangju_1_4 WHERE Ranking LIKE 3 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Gyeongbuk_1_4 WHERE Ranking LIKE 3 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Gyeonggi_1_4 WHERE Ranking LIKE 3 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Gyeongnam_1_4 WHERE Ranking LIKE 3 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Incheon_1_4 WHERE Ranking LIKE 3 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Jeju_1_4 WHERE Ranking LIKE 3 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Jeonbuk_1_4 WHERE Ranking LIKE 3 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Jeonnam_1_4 WHERE Ranking LIKE 3 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Sejong_1_4 WHERE Ranking LIKE 3 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Seoul_1_4 WHERE Ranking LIKE 3 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Ulsan_1_4 WHERE Ranking LIKE 3 ");
+            calsend2();
+        }
+        else if (MovieName == "명탐정 코난: 진홍의 수학여행")
+        {
+            DataBaseThisRead("Select * From All_1_4 WHERE Ranking LIKE 4 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Busan_1_4 WHERE Ranking LIKE 4 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Chungbuk_1_4 WHERE Ranking LIKE 4 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Chungnam_1_4 WHERE Ranking LIKE 4 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Daegu_1_4 WHERE Ranking LIKE 4 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Daejeon_1_4 WHERE Ranking LIKE 4 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Gangwon_1_4 WHERE Ranking LIKE 4 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Gwangju_1_4 WHERE Ranking LIKE 4 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Gyeongbuk_1_4 WHERE Ranking LIKE 4 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Gyeonggi_1_4 WHERE Ranking LIKE 4 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Gyeongnam_1_4 WHERE Ranking LIKE 4 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Incheon_1_4 WHERE Ranking LIKE 4 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Jeju_1_4 WHERE Ranking LIKE 4 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Jeonbuk_1_4 WHERE Ranking LIKE 4 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Jeonnam_1_4 WHERE Ranking LIKE 4 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Sejong_1_4 WHERE Ranking LIKE 4 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Seoul_1_4 WHERE Ranking LIKE 4 ");
+            calsend2();
+
+            DataBaseThisRead("Select * From Ulsan_1_4 WHERE Ranking LIKE 4 ");
+            calsend2();
+        }
     }
 
+
+    public void calsend2()
+    {
+        calculate.setThisSalesShare(ThisSalesShare);
+        calculate.setAudience(Audience);
+        calculate.setScreen(Screen);
+    }
 
     void Update()
     {
-        StartCoroutine(DBCreate());
-    }
 
-    #region DB생성
-    IEnumerator DBCreate()
-    {
-        string filepath = string.Empty;
-        if (Application.platform == RuntimePlatform.Android)
-        {
-            filepath = Application.persistentDataPath + "/DB.db";
-            if (!File.Exists(filepath))
-            {
-                UnityWebRequest unityWebRequest = UnityWebRequest.Get("jar:file://" + Application.dataPath + "!/Assets/DB.db");
-                unityWebRequest.downloadedBytes.ToString();
-                yield return unityWebRequest.SendWebRequest().isDone;
-                File.WriteAllBytes(filepath, unityWebRequest.downloadHandler.data);
-            }
-        }
-        else
-        {
-            filepath = Application.dataPath + "/DB.db";
-            if (!File.Exists(filepath))
-            {
-                File.Copy(Application.streamingAssetsPath + "/DB.db", filepath);
-            }
-        }
-        Debug.Log("생성 완료");
     }
-    #endregion
     #region DB파일 경로
     public string GetDBFilePath()
     {
@@ -68,7 +285,6 @@ public class DB : MonoBehaviour
     {
         try
         {
-
             IDbConnection dbConnection = new SqliteConnection(GetDBFilePath());
             dbConnection.Open();
             if (dbConnection.State == ConnectionState.Open)
@@ -87,7 +303,7 @@ public class DB : MonoBehaviour
     }
     #endregion
     #region DB 읽기
-    public void DataBaseRead(string query)
+    public void DataBaseBeginRead(string query)
     {
         IDbConnection dbConnection = new SqliteConnection(GetDBFilePath());
         dbConnection.Open();
@@ -98,7 +314,43 @@ public class DB : MonoBehaviour
         {
             Debug.Log(dataReader.GetInt32(0) + "," + dataReader.GetString(1) + "," + dataReader.GetString(2) + ","
                 + dataReader.GetFloat(3) + "," + dataReader.GetInt32(4) + "," + dataReader.GetInt32(5));
+
+            if (dataReader.GetString(1) == "소울")
+            {
+                a = 1;
+            }
+           
+            
+            switch (a)
+            {
+                case 1:
+                    BeginSalesShare = dataReader.GetFloat(3);
+                    Audience = dataReader.GetInt32(4);
+                    Screen = dataReader.GetInt32(5);
+                    break;
+                case 2:
+                    BeginSalesShare = dataReader.GetFloat(3);
+                    Audience = dataReader.GetInt32(4);
+                    Screen = dataReader.GetInt32(5);
+                    break;
+                case 3:
+                    BeginSalesShare = dataReader.GetFloat(3);
+                    Audience = dataReader.GetInt32(4);
+                    Screen = dataReader.GetInt32(5);
+                    break;
+                case 4:
+                    BeginSalesShare = dataReader.GetFloat(3);
+                    Audience = dataReader.GetInt32(4);
+                    Screen = dataReader.GetInt32(5);
+                    break;
+                default:
+                    break;
+            }
+            Debug.Log(dataReader.GetFloat(3)+""+dataReader.GetInt32(4)+""+dataReader.GetInt32(5));
+            
         }
+
+
         dataReader.Dispose();
         dataReader = null;
         dbCommand.Dispose();
@@ -107,5 +359,70 @@ public class DB : MonoBehaviour
         dbConnection = null;
     }
     #endregion
+    public void DataBaseThisRead(string query)
+    {
+        IDbConnection dbConnection = new SqliteConnection(GetDBFilePath());
+        dbConnection.Open();
+        IDbCommand dbCommand = dbConnection.CreateCommand();
+        dbCommand.CommandText = query;
+        IDataReader dataReader = dbCommand.ExecuteReader();
+        while (dataReader.Read())
+        {
+            Debug.Log(dataReader.GetInt32(0) + "," + dataReader.GetString(1) + "," + dataReader.GetString(2) + ","
+                + dataReader.GetFloat(3) + "," + dataReader.GetInt32(4) + "," + dataReader.GetInt32(5));
 
+            if (dataReader.GetString(1) == "소울")
+            {
+                a = 1;
+            }
+            else if (dataReader.GetString(1) == "극장판 귀멸의 칼날: 무한열차편")
+            {
+                a = 2;
+            }
+            else if (dataReader.GetString(1) == "세자매")
+            {
+                a = 3;
+            }
+            else if (dataReader.GetString(1) == "명탐정 코난: 진홍의 수학여행")
+            {
+                a = 4;
+            }
+
+            switch (a)
+            {
+                case 1:
+                    ThisSalesShare = dataReader.GetFloat(3);
+                    Audience = dataReader.GetInt32(4);
+                    Screen = dataReader.GetInt32(5);
+                    break;
+                case 2:
+                    ThisSalesShare = dataReader.GetFloat(3);
+                    Audience = dataReader.GetInt32(4);
+                    Screen = dataReader.GetInt32(5);
+                    break;
+                case 3:
+                    ThisSalesShare = dataReader.GetFloat(3);
+                    Audience = dataReader.GetInt32(4);
+                    Screen = dataReader.GetInt32(5);
+                    break;
+                case 4:
+                    ThisSalesShare = dataReader.GetFloat(3);
+                    Audience = dataReader.GetInt32(4);
+                    Screen = dataReader.GetInt32(5);
+                    break;
+                default:
+                    break;
+            }
+            Debug.Log(dataReader.GetFloat(3) + "" + dataReader.GetInt32(4) + "" + dataReader.GetInt32(5));
+
+        }
+
+
+        dataReader.Dispose();
+        dataReader = null;
+        dbCommand.Dispose();
+        dbCommand = null;
+        dbConnection.Close();
+        dbConnection = null;
+    }
 }
