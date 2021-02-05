a17c05e6b73a0fb7698bfdfcd4175a3a660062e7
KitchenGun <kitchengun124@gmail.com>
2021-02-05 13:42:20 +0900

    movie 클래스 생성
    영화의 정보를 담을 객체

---
+++ RunningDate/Assets/Scripts/Front-end/MovieInfo/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movie : MonoBehaviour
+{
+    public string Image { get; private set; }
+    public string Name { get; private set; }
+    public int UserRating { get; private set; }
+    public string DirectorName { get; private set; }
+
+    private void DataUpdate()
+    {
+        
+    }
+}
